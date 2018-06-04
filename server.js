240a5cb6636d72d8a6c5fd25e78e4d6322c21535
adventurebear <suzanneatkinson@gmail.com>
2018-06-04 13:49:25 -0400

    update server file

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use(express.static('build'));
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 //   });
 
-app.get("/", function (req,res){
+app.get("/*", function (req,res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
